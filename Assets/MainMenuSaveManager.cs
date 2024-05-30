a3b3b43d49ef7e97acb0897a90762d720a0ecd8b
Zeynep Kübra Batmaz <z.k.batmaz@gmail.com>
2024-05-30 23:30:08 +0300

    Create MainMenuSaveManager.cs

---
+++ Assets/MainMenuSaveManager.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuSaveManager : MonoBehaviour
+{
+    public static MainMenuSaveManager Instance { get; set; }
+
+    private void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            Instance = this;
+        }
+    }
+
+    [System.Serializable]
+    public class VolumeSettings
+    {
+        public float music;
+        public float effects;
+        public float master;
+    }
+
+    public void SaveVolumeSettings(float _music, float _effects, float _master)
+    {
+        VolumeSettings volumeSettings = new VolumeSettings()
+        {
+            music = _music,
+            effects = _effects,
+            master = _master
+        };
+
+        PlayerPrefs.SetString("Volume", JsonUtility.ToJson(volumeSettings));
+        PlayerPrefs.Save();
+
+        print("Saved to Player Pref.");
+    }
+
+    public VolumeSettings LoadVolumeSettings()
+    {
+        return JsonUtility.FromJson<VolumeSettings>(PlayerPrefs.GetString("Volume"));
+        
+    }
+}
