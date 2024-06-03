224602d648ed912f1616168e90f77b4dac0c0657
Beria Ayas <melikeberiaayas@gmail.com>
2024-06-03 20:41:10 +0300

    28

--- Assets/NPC.cs
+++ Assets/NPC.cs
@@ -8,6 +8,371 @@ public class NPC : MonoBehaviour
     public bool playerInRange;
     public bool isTalkingWithPlayer;
 
+
+    TextMeshProUGUI npcDialogText;
+
+    Button optionButton1;
+    TextMeshProUGUI optionButton1Text;
+
+    Button optionButton2;
+    TextMeshProUGUI optionButton2Text;
+
+    public List<Quest> quests;
+    public Quest currentActiveQuest = null;
+    public int activeQuestIndex = 0;
+    public bool firstTimeInteraction = true;
+    public int currentDialog;
+
+
+
+
+
+    private void Start()
+    {
+        npcDialogText = DialogSystem.Instance.dialogText;
+
+        optionButton1 = DialogSystem.Instance.option1BTN;
+        optionButton1Text = DialogSystem.Instance.option1BTN.transform.Find("Text (TMP)").GetComponent<TextMeshProUGUI>();
+
+        optionButton2 = DialogSystem.Instance.option2BTN;
+        optionButton2Text = DialogSystem.Instance.option2BTN.transform.Find("Text (TMP)").GetComponent<TextMeshProUGUI>();
+    }
+
+
+    public void StartConversation()
+    {
+        isTalkingWithPlayer = true;
+
+        LookAtPlayer();
+
+
+        if (firstTimeInteraction)
+        {
+            firstTimeInteraction = false;
+            currentActiveQuest = quests[activeQuestIndex];
+            StartQuestInitialDialog();
+            currentDialog = 0;
+        }
+        else
+        {
+
+            if (currentActiveQuest.declined)
+            {
+
+                DialogSystem.Instance.OpenDialogUI();
+
+                npcDialogText.text = currentActiveQuest.info.comebackAfterDecline;
+
+                SetAcceptAndDeclineOptions();
+
+
+            }
+
+
+            if (currentActiveQuest.accepted && currentActiveQuest.isCompleted == false)
+            {
+                if (AreQuestRequirmentsCompleted())
+                {
+                    SubmitRequiredItems();
+
+                    DialogSystem.Instance.OpenDialogUI();
+
+                    npcDialogText.text = currentActiveQuest.info.comebackCompleted;
+
+                    optionButton1Text.text = "[Take Reward]";
+                    optionButton1.onClick.RemoveAllListeners();
+                    optionButton1.onClick.AddListener(() =>
+                    {
+                        ReceiveRewardAndCompleteQuest();
+                    });
+                }
+                else
+                {
+
+                    DialogSystem.Instance.OpenDialogUI();
+
+                    npcDialogText.text = currentActiveQuest.info.comebackInProgress;
+
+                    optionButton1Text.text = "[Close]";
+                    optionButton1.onClick.RemoveAllListeners();
+                    optionButton1.onClick.AddListener(() =>
+                    {
+                        DialogSystem.Instance.CloseDialogUI();
+                        isTalkingWithPlayer = false;
+                    });
+                }
+            }
+
+
+
+            if (currentActiveQuest.isCompleted == true)
+            {
+                DialogSystem.Instance.OpenDialogUI();
+
+                npcDialogText.text = currentActiveQuest.info.finalWords;
+
+                optionButton1Text.text = "[Close]";
+                optionButton1.onClick.RemoveAllListeners();
+                optionButton1.onClick.AddListener(() =>
+                {
+                    DialogSystem.Instance.CloseDialogUI();
+                    isTalkingWithPlayer = false;
+                });
+            }
+
+
+            if (currentActiveQuest.initialDialogCompleted == false)
+            {
+                StartQuestInitialDialog();
+            }
+
+        }
+    }
+
+
+
+    private void SetAcceptAndDeclineOptions()
+    {
+        optionButton1Text.text = currentActiveQuest.info.acceptOption;
+        optionButton1.onClick.RemoveAllListeners();
+        optionButton1.onClick.AddListener(() =>
+        {
+            AcceptedQuest();
+        });
+
+
+        optionButton2.gameObject.SetActive(true);
+        optionButton2Text.text = currentActiveQuest.info.declineOption;
+        optionButton2.onClick.RemoveAllListeners();
+        optionButton2.onClick.AddListener(() =>
+        {
+            DeclinedQuest();
+        });
+    }
+
+
+
+    private void SubmitRequiredItems()
+    {
+        string firstRequiredItem = currentActiveQuest.info.firstRequirmentItem;
+        int firstRequiredAmount = currentActiveQuest.info.firstRequirementAmount;
+
+        if(firstRequiredItem != "")
+        {
+            InventorySystem.Instance.RemoveItem(firstRequiredItem, firstRequiredAmount);
+
+        }
+
+        string secondRequiredItem = currentActiveQuest.info.secondRequirmentItem;
+        int secondRequiredAmount = currentActiveQuest.info.secondRequirementAmount;
+
+        if(firstRequiredItem != "")
+        {
+            InventorySystem.Instance.RemoveItem(secondRequiredItem, secondRequiredAmount);
+        }
+    }
+
+
+            private bool AreQuestRequirmentsCompleted()
+            {
+                print("Checking Requirments");
+
+
+                string firstRequiredItem = currentActiveQuest.info.firstRequirmentItem;
+                int firstRequiredAmount = currentActiveQuest.info.firstRequirementAmount;
+
+                var firstItemCounter = 0;
+
+                foreach(string item in InventorySystem.Instance.itemList)
+                {
+                    if(item == firstRequiredItem)
+                    {
+                        firstItemCounter++;
+                    }
+                }
+
+
+                string secondRequiredItem = currentActiveQuest.info.secondRequirmentItem;
+                int secondRequiredAmount = currentActiveQuest.info.secondRequirementAmount;
+
+                var secondItemCounter = 0;
+
+                foreach(string item in InventorySystem.Instance.itemList)
+                {
+                    if (item == secondRequiredItem)
+                    {
+                        secondItemCounter++;
+                    }
+                }
+
+                if(firstItemCounter >= firstRequiredAmount && secondItemCounter >= secondRequiredAmount)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+
+
+
+
+
+
+    private void StartQuestInitialDialog()
+    {
+        DialogSystem.Instance.OpenDialogUI();
+
+        npcDialogText.text = currentActiveQuest.info.initialDialog[currentDialog];
+        optionButton1Text.text = "Next";
+        optionButton1.onClick.RemoveAllListeners();
+        optionButton1.onClick.AddListener(() =>
+        {
+            currentDialog++;
+            CheckIfDialogDone();
+        });
+
+
+        optionButton2.gameObject.SetActive(false);
+    }
+
+
+    private void CheckIfDialogDone()
+    {
+        if(currentDialog == currentActiveQuest.info.initialDialog.Count - 1)
+        {
+            npcDialogText.text = currentActiveQuest.info.initialDialog[currentDialog];
+
+            currentActiveQuest.initialDialogCompleted = true;
+
+            SetAcceptAndDeclineOptions();
+
+        }
+        else
+        {
+            npcDialogText.text = currentActiveQuest.info.initialDialog[currentDialog];
+
+            optionButton1Text.text = "Next";
+            optionButton1.onClick.RemoveAllListeners();
+            optionButton1.onClick.AddListener(() =>
+            {
+                currentDialog++;
+                CheckIfDialogDone();
+            });
+        }
+    }
+
+
+
+
+    private void AcceptedQuest()
+    {
+        currentActiveQuest.accepted = true;
+        currentActiveQuest.declined = false;
+
+        if(currentActiveQuest.hasNoRequirements)
+        {
+            npcDialogText.text = currentActiveQuest.info.comebackCompleted;
+            optionButton1Text.text = "[Take Reward]";
+            optionButton1.onClick.RemoveAllListeners();
+            optionButton1.onClick.AddListener(() =>
+            {
+                ReceiveRewardAndCompleteQuest();
+            });
+            optionButton2.gameObject.SetActive(false);
+        }
+        else
+        {
+            npcDialogText.text = currentActiveQuest.info.acceptAnswer;
+            CloseDialogUI();
+         
+        }
+    }
+
+
+    private void CloseDialogUI()
+    {
+        optionButton1Text.text = "[Close]";
+        optionButton1.onClick.RemoveAllListeners();
+        optionButton1.onClick.AddListener(() =>
+        {
+            DialogSystem.Instance.CloseDialogUI();
+            isTalkingWithPlayer = false;
+        });
+        optionButton2.gameObject.SetActive(false);
+    }
+
+
+    private void ReceiveRewardAndCompleteQuest()
+    {
+        currentActiveQuest.isCompleted = true;
+
+        var coinsRecieved = currentActiveQuest.info.coinReward;
+        print("You received " + coinsRecieved + " gold coins");
+
+        if(currentActiveQuest.info.rewardItem1 != "")
+        {
+            InventorySystem.Instance.AddToInventory(currentActiveQuest.info.rewardItem1);
+        }
+        if(currentActiveQuest.info.rewardItem2 != "")
+        {
+            InventorySystem.Instance.AddToInventory(currentActiveQuest.info.rewardItem2);
+        }
+
+        activeQuestIndex++;
+
+
+        if(activeQuestIndex < quests.Count)
+        {
+            currentActiveQuest = quests[activeQuestIndex];
+            currentDialog = 0;
+            DialogSystem.Instance.CloseDialogUI();
+            isTalkingWithPlayer = false;
+        }
+        else
+        {
+            DialogSystem.Instance.CloseDialogUI();
+            isTalkingWithPlayer = false;
+            print("No more quests");
+        }
+    }
+
+
+
+    private void DeclinedQuest()
+    {
+        
+        currentActiveQuest.declined = true;
+
+       
+        npcDialogText.text = currentActiveQuest.info.declineAnswer;
+        CloseDialogUI();
+      
+       
+    }
+
+
+
+
+
+
+    public void LookAtPlayer()
+    {
+        var player = PlayerState.Instance.playerBody.transform;
+        Vector3 direction = player.position - transform.position;
+        transform.rotation = Quaternion.LookRotation(direction);
+
+
+        var yRotation = transform.eulerAngles.y;
+        transform.rotation = Quaternion.Euler(0, yRotation, 0);
+    }
+
+
+
+
+
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
@@ -19,20 +384,5 @@ public class NPC : MonoBehaviour
     {
         if (other.CompareTag("Player"))
         { playerInRange = false; }
-    }
-    public void StartConversation()
-    {
-        isTalkingWithPlayer = true;
-        print("Conversation Started");
-
-
-        DialogSystem.Instance.OpenDialogUI();
-        DialogSystem.Instance.dialogText.text = "Hello There";
-        DialogSystem.Instance.option1BTN.transform.Find("Text (TMP)").GetComponent<TextMeshProUGUI>().text="Bye";
-        DialogSystem.Instance.option1BTN.onClick.AddListener(() =>
-        {
-            DialogSystem.Instance.CloseDialogUI();
-            isTalkingWithPlayer=false;
-        });
     }
 }
