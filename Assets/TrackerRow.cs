3c995154a4ad599cfa4f4121ce6afefd8adfec29
Zeynep Kübra Batmaz <z.k.batmaz@gmail.com>
2024-06-06 23:33:27 +0300

    Create TrackerRow.cs

---
+++ Assets/TrackerRow.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class TrackerRow : MonoBehaviour
+{
+    public TextMeshProUGUI questName;
+    public TextMeshProUGUI description;
+    public TextMeshProUGUI requirements;
+}
